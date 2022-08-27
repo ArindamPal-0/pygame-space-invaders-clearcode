36232ae9f59385127bbf3daa5be25310252028ad
Arindam Pal <arindampal2000@gmail.com>
2022-08-27 08:31:51 +0530

    basic pygame setup - main window, game loop, checking in event loop, capping framerate

--- main.py
+++ main.py
@@ -1,9 +1,34 @@
-import pygame
+import pygame, sys
 
 def main() -> int:
+    # initializing pygame
     pygame.init()
+
+    # screen width and height
+    screen_width: int = 600
+    screen_height: int = 600
+
+    # creating a display surface
+    screen: pygame.Surface = pygame.display.set_mode((screen_width, screen_height))
+    clock = pygame.time.Clock()
+
+    while True:
+        for event in pygame.event.get():
+            # checking for exit event
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                return 0
+        
+        # giving a simple background color
+        screen.fill((30, 30, 30))
+
+        # displaying it on screen
+        pygame.display.flip()
+
+        # used to limit the framerate to 60 Hz
+        clock.tick(60)
 
     return 0
 
 if __name__ == "__main__":
-    exit(main())
+    sys.exit(main())
